121fbfe38442bd57417eebf33cff0c038e4c9a03
hagar <hagarnabil7@gmail.com>
2024-08-06 18:11:41 +0300

    deploying to cf

---
+++ src/main/java/com/example/btpsd/config/MySQLCloudConfig.java
@@ -0,0 +1,19 @@
+package com.example.btpsd.config;
+
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Profile("cloud")
+@Configuration
+public class MySQLCloudConfig extends AbstractCloudConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
+
+}
