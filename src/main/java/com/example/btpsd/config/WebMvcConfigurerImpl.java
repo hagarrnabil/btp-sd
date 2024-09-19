6bde2d22538baa5a79d41a97375f10f80a37212e
youssef <youssefhassanshapan1995@gmail.com>
2024-09-19 16:32:10 +0300

    Adding Swagger Ui And Configurations And Comment All Security Dependencies

--- src/main/java/com/example/btpsd/config/WebMvcConfigurerImpl.java
+++ src/main/java/com/example/btpsd/config/WebMvcConfigurerImpl.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
 @Configuration
-public class WebMvcConfigurerImpl extends WebMvcConfigurationSupport {
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
 
 
     @Bean
