7dd603e3067dbbf7c81364f0b3695a3ac5648982
youssef <youssefhassanshapan1995@gmail.com>
2024-09-19 16:50:04 +0300

    Deleting Commented Unused Security Dependencies and classes

--- src/main/java/com/example/btpsd/repositories/security/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.btpsd.repositories.security;
-//
-//import com.example.btpsd.model.security.Authority;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
-//}
