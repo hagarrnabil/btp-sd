0c44dcc2c7c5248de20744744a65bb1d494cda05
youssef <youssefhassanshapan1995@gmail.com>
2024-09-26 17:40:48 +0300

    Last Stable Sucess

--- src/main/java/com/example/btpsd/BtpSdApplication.java
+++ src/main/java/com/example/btpsd/BtpSdApplication.java
@@ -26,14 +26,14 @@ public class BtpSdApplication {
 
 
     }
-    @Bean
-    public FilterRegistrationBean<RequestLoggingFilter> loggingFilter() {
-        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new RequestLoggingFilter());
-        registrationBean.setName("request-logging");
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setDispatcherTypes(DispatcherType.REQUEST);
-        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registrationBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean<RequestLoggingFilter> loggingFilter() {
+//        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new RequestLoggingFilter());
+//        registrationBean.setName("request-logging");
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+//        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return registrationBean;
+//    }
 }
