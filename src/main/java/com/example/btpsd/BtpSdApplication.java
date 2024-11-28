ec47d944e3602f8427cdfebcc153c2897d4059b5
youssef <youssefhassanshapan1995@gmail.com>
2024-11-28 16:34:38 +0200

    add new configs

--- src/main/java/com/example/btpsd/BtpSdApplication.java
+++ src/main/java/com/example/btpsd/BtpSdApplication.java
@@ -6,29 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Library API",
-                version = "1.0",
-                description = "Library Management APIs"
-        )
-)
 public class BtpSdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BtpSdApplication.class, args);
 
-
-
     }
-//    @Bean
-//    public FilterRegistrationBean<RequestLoggingFilter> loggingFilter() {
-//        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
-//        registrationBean.setFilter(new RequestLoggingFilter());
-//        registrationBean.setName("request-logging");
-//        registrationBean.addUrlPatterns("/*");
-//        registrationBean.setDispatcherTypes(DispatcherType.REQUEST);
-//        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//        return registrationBean;
-//    }
 }
--- src/main/java/com/example/btpsd/config/SwaggerUiConfig.java
+++ src/main/java/com/example/btpsd/config/SwaggerUiConfig.java
@@ -5,11 +5,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.NonNull;
+
 @EnableWebMvc
 @Configuration
 public class SwaggerUiConfig implements WebMvcConfigurer {
 
     @Override
+    @NonNull
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
@@ -17,6 +20,4 @@ public class SwaggerUiConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
-
 }
